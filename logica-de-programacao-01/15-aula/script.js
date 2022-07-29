5fc2b6f7974916ebeaf4dd20be3d47c6096fe9d0
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-07-29 19:01:30 -0300

    Trabalhando com if

--- logica-de-programacao-01/15-aula/script.js
+++ logica-de-programacao-01/15-aula/script.js
@@ -10,25 +10,11 @@ function mostra(frase) {
   pulaLinha();
 }
 
-function calculaImc(altura, peso) {
+var numeroPensado = 5;
+var chute = parseInt(prompt("Digite seu chute !!!"))
 
-  return peso / (altura * altura);
-}
-
-var nome = prompt("Informe o seu nome");
-var alturaInformada = prompt(nome + ", informe sua altura");
-var pesoInformado = prompt(nome + ", informe seu peso");
-
-var imc = calculaImc(alturaInformada, pesoInformado);
-
-document.write(nome + ", o seu IMC é " + imc);
-
-if(imc < 18.5){
-  mostra("Você está abaixo do recomendado !");
-}
-if(imc >= 18.5){
-  if(imc <= 35.5){
-    mostra("Seu Imc está excelente !");
-  }
-
+if(chute == numeroPensado){
+  document.write("você acertou !");
+}else{
+  document.write("você errou, o numero pensado foi"+ numeroPensado)
 }
