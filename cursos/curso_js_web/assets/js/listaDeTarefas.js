f7164655121ad188ad864a3e70e3cd0d84a21d9e
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-06 23:58:26 -0300

    Trabalhando com Eventos

---
+++ cursos/curso_js_web/assets/js/listaDeTarefas.js
@@ -0,0 +1,7 @@
+// Pegar o atributo -> data-form-button
+const novaTarefa = document.querySelector('[data-form-button]');
+
+//Evento de Click
+novaTarefa.addEventListener('click', ()=>{ 
+    console.log("Fui Clicado")
+})
