3f571db7392f56ffd5b8429f8f692fc87f5dc16d
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-13 16:37:56 -0300

    Criando lista de eventos com Javascript

--- cursos/Javascript/curso_js_web/assets/js/listaDeTarefas.js
+++ cursos/Javascript/curso_js_web/assets/js/listaDeTarefas.js
@@ -1,11 +1,14 @@
+const criarTarefa = (evento) => {
+     // prevenir comportamento padrão
+    evento.preventDefault();
+    const input =  document.querySelector('[data-form-input]');
+    const valor = input.value
+    console.log(valor)
+    input.value = ""
+}
+
 // Pegar o atributo -> data-form-button
 const novaTarefa = document.querySelector('[data-form-button]');
 
-
 //Evento de Click
-novaTarefa.addEventListener('click', ()=>{ 
-    const input =  document.querySelector('[data-form-input]');
-    const valor = input.value
-    
-    console.log(valor)
-})
+novaTarefa.addEventListener('click', criarTarefa)
