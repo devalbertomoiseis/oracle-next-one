b478d95ee0a1e406f9100c926870b4b81c134cb8
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-12 22:22:32 -0300

    Estudando Javascript

---
+++ cursos/Javascript/ExplorandoJavascript/01-Modulo/variaveis.js
@@ -0,0 +1,10 @@
+let nome = "alberto ";
+let sobreNome = "moiseis";
+const sexo = "M";
+let idade = 31;
+let estuda = true;
+console.log(typeof(nome));
+console.log(typeof(sobreNome));
+console.log(typeof(sexo));
+console.log(typeof(idade));
+console.log(typeof(estuda));
